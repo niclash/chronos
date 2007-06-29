fdc397affaca89c7fb0eb2e41f867c7d81fc4269
boon <boon@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-29 05:05:44 +0000

    o Added copyright
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/apps/chronos@7151 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- core/src/main/java/org/qi4j/chronos/model/composites/AccountPersistentComposite.java
+++ core/src/main/java/org/qi4j/chronos/model/composites/AccountPersistentComposite.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2007 Lan Boon Ping. All Rights Reserved.
+ * Copyright (c) 2007, Sianny Halim. All Rights Reserved.
+ * Copyright (c) 2007, Lan Boon Ping. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,7 +10,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package org.qi4j.chronos.model.composites;
 
 import org.qi4j.api.annotation.ImplementedBy;
--- core/src/main/java/org/qi4j/chronos/model/mixins/RoleMixin.java
+++ core/src/main/java/org/qi4j/chronos/model/mixins/RoleMixin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Lan Boon Ping. All Rights Reserved.
+ * Copyright (c) 2007, Sianny Halim. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package org.qi4j.chronos.model.mixins;
 
 import org.qi4j.api.annotation.ModifiedBy;
