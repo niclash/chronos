ce9c2627c3e1e19a5fddba398ef1f4e358092561
siannyhalim@gmail.com <siannyhalim@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-07-18 09:33:03 +0000

    Add mixin implementation of HasProject.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/apps/chronos@7340 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ ui/src/main/java/org/qi4j/ui/association/mixins/HasProjectMixin.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2007, Sianny Halim. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qi4j.chronos.ui.association.mixins;
+
+import org.qi4j.chronos.model.Project;
+import org.qi4j.chronos.ui.association.HasProject;
+
+public final class HasProjectMixin implements HasProject
+{
+    private Project project;
+
+    public Project getProject()
+    {
+        return project;
+    }
+
+    public void setProject( Project aProject )
+    {
+        project = aProject;
+    }
+}
