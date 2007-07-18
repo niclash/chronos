2a70db0efd9b9411f91e58b951196c9203562ac2
siannyhalim@gmail.com <siannyhalim@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-07-18 09:56:58 +0000

    Add Value and mixin implementation for ServletLifecycle
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/apps/chronos@7346 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ ui/src/main/java/org/qi4j/ui/mixins/ServletLifecycleMixin.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2007, Sianny Halim. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qi4j.ui.mixins;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.qi4j.ui.ServletLifecycle;
+import org.qi4j.ui.RenderFailedException;
+import org.qi4j.ui.SubmitFailedException;
+
+public final class ServletLifecycleMixin implements ServletLifecycle
+{
+    public void render( HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse ) throws RenderFailedException
+    {
+        // Actions performed by modifiers
+    }
+
+    public void submit( HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse ) throws SubmitFailedException
+    {
+        // Actions performed by modifiers
+    }
+}
