693f32e220afef4cd32b32b7bea4f4ac9182425d
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-10-01 11:43:09 +0000

    Reworked Query API Added Calendar example (needs to be refactored into its own module later)
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/apps/chronos@8772 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- core/src/test/java/org/qi4j/chronos/model/AbstractTest.java
+++ core/src/test/java/org/qi4j/chronos/model/AbstractTest.java
@@ -7,10 +7,10 @@ import org.qi4j.api.persistence.IdentityGenerator;
 import org.qi4j.extension.persistence.quick.MapPersistenceProvider;
 import org.qi4j.extension.persistence.quick.SerializablePersistence;
 import org.qi4j.runtime.CompositeBuilderFactoryImpl;
-import org.qi4j.runtime.UuidIdentityGenerator;
 import org.qi4j.runtime.CompositeModelFactory;
-import org.qi4j.runtime.ModifierModelFactory;
 import org.qi4j.runtime.MixinModelFactory;
+import org.qi4j.runtime.ModifierModelFactory;
+import org.qi4j.runtime.UuidIdentityGenerator;
 import org.qi4j.runtime.persistence.EntitySessionImpl;
 import org.qi4j.spi.serialization.SerializablePersistenceSpi;
 
@@ -28,6 +28,6 @@ public abstract class AbstractTest extends TestCase
         IdentityGenerator identityGenerator = new UuidIdentityGenerator();
         SerializablePersistenceSpi subsystem = new MapPersistenceProvider();
         SerializablePersistence storage = new SerializablePersistence( subsystem, modelFactory, builderFactory );
-        session = new EntitySessionImpl( storage, modelFactory, builderFactory, identityGenerator );
+        session = new EntitySessionImpl( storage, builderFactory, identityGenerator );
     }
 }
