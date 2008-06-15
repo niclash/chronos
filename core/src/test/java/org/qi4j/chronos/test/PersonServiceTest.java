b0c80d8c0ffe576400c86dc9544ac4488eee80dc
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-06-15 22:06:54 +0000

    o Fixing up testcases.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/apps/chronos@12002 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- core/src/test/java/org/qi4j/chronos/test/PersonServiceTest.java
+++ core/src/test/java/org/qi4j/chronos/test/PersonServiceTest.java
@@ -12,14 +12,12 @@
  */
 package org.qi4j.chronos.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import org.junit.Before;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.qi4j.bootstrap.AssemblyException;
 import org.qi4j.bootstrap.ModuleAssembly;
 import org.qi4j.entity.EntityCompositeNotFoundException;
-import org.qi4j.entity.UnitOfWorkCompletionException;
+import org.qi4j.entity.UnitOfWork;
 import org.qi4j.entity.memory.MemoryEntityStoreService;
 import org.qi4j.object.ObjectBuilder;
 import org.qi4j.spi.entity.UuidIdentityGeneratorService;
@@ -29,83 +27,75 @@ public class PersonServiceTest extends AbstractCommonTest
     @Override public void assemble( ModuleAssembly module ) throws AssemblyException
     {
         module.addObjects( Items.class );
-        module.addEntities( PersonEntity.class, PersonServiceConfiguration.class);
+        module.addEntities( PersonEntity.class, PersonServiceConfiguration.class );
         module.addComposites( ItemCompositeToo.class );
         module.addServices( PersonServiceComposite.class, UuidIdentityGeneratorService.class, MemoryEntityStoreService.class );
     }
 
     @Test
-    public void personTest() throws Exception
+    public void personTest()
+        throws Exception
     {
-        PersonServiceComposite personService = serviceLocator.findService( PersonServiceComposite.class ).get();
-
-        ObjectBuilder<Items> objectBuilder = unitOfWork.objectBuilderFactory().newObjectBuilder( Items.class );
-        Items item = objectBuilder.newInstance();
-        item.set( "test" );
-
-        PersonEntity p = personService.newInstance( unitOfWork );
-        p.name().set( "test" );
-        p.item().set( item );
-        personService.save( p );
-
-        PersonEntity p2 = personService.newInstance( unitOfWork );
-        p2.name().set( "another test" );
-        p2.item().set( item );
-        personService.save( p2 );
-
-        String id = p2.identity().get();
-
+        UnitOfWork uow = unitOfWorkFactory.newUnitOfWork();
         try
         {
-            unitOfWork.complete();
+            PersonServiceComposite personService = serviceLocator.findService( PersonServiceComposite.class ).get();
+
+            ObjectBuilder<Items> objectBuilder = uow.objectBuilderFactory().newObjectBuilder( Items.class );
+            Items item = objectBuilder.newInstance();
+            item.set( "test" );
+
+            PersonEntity p = personService.newInstance( uow );
+            p.name().set( "test" );
+            p.item().set( item );
+            personService.save( p );
+
+            PersonEntity p2 = personService.newInstance( uow );
+            p2.name().set( "another test" );
+            p2.item().set( item );
+            personService.save( p2 );
+
+            String id = p2.identity().get();
+            uow.complete();
+
+            assertEquals( "Size not equals to 2", 2, personService.count() );
+
+            for( PersonEntity person : personService.findAll() )
+            {
+                System.out.println( person.name().get() );
+                System.out.println( person.item().get() );
+            }
+
+            uow = unitOfWorkFactory.newUnitOfWork();
+
+            PersonEntity deletePerson = uow.dereference( p2 );
+            personService.delete( uow, deletePerson );
+            uow.complete();
+
+            assertEquals( "Size not equals to 1", 1, personService.count() );
+
+            for( PersonEntity person : personService.findAll() )
+            {
+                System.out.println( person.name().get() );
+            }
+
+            uow = unitOfWorkFactory.newUnitOfWork();
+
+            try
+            {
+                PersonEntity findPerson = uow.find( id, PersonEntity.class );
+                System.out.println( findPerson.name().get() );
+                fail( "Should not be able to find entity" );
+            }
+            catch( EntityCompositeNotFoundException ecnfe )
+            {
+                assertEquals( "This is expected", 1, 1 );
+            }
         }
-        catch( UnitOfWorkCompletionException uowce )
+        catch( RuntimeException e )
         {
-            System.err.println( uowce.getLocalizedMessage() );
-            uowce.printStackTrace();
-        }
-
-        assertEquals( "Size not equals to 2", 2, personService.count() );
-
-        for( PersonEntity person : personService.findAll() )
-        {
-            System.out.println( person.name().get() );
-            System.out.println( person.item().get() );
-        }
-
-        unitOfWork = unitOfWorkFactory.newUnitOfWork();
-
-        PersonEntity deletePerson = unitOfWork.dereference( p2 );
-        personService.delete( unitOfWork, deletePerson );
-
-        try
-        {
-            unitOfWork.complete();
-        }
-        catch( UnitOfWorkCompletionException uowce )
-        {
-            System.err.println( uowce.getLocalizedMessage() );
-            uowce.printStackTrace();
-        }
-
-        assertEquals( "Size not equals to 1", 1, personService.count() );
-
-        for( PersonEntity person : personService.findAll() )
-        {
-            System.out.println( person.name().get() );
-        }
-
-        unitOfWork = unitOfWorkFactory.newUnitOfWork();
-
-        try
-        {
-            PersonEntity findPerson = unitOfWork.find( id, PersonEntity.class );
-            System.out.println( findPerson.name().get() );
-            fail( "Should not be able to find entity" );
-        }
-        catch( EntityCompositeNotFoundException ecnfe )
-        {
-            assertEquals( "This is expected", 1, 1 );
+            uow.discard();
+            throw e;
         }
     }
 }
