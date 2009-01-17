d46e14c4e62cde2143fef8fc0c68900f6c03d71c
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-01-17 16:35:53 +0000

    o Adding a Enumeration library, for management of 'typed collection of instances'.
    o Trying to find usage patterns in the general library, which makes it useful to clients.
    
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/apps/chronos@14019 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- core/src/main/java/org/qi4j/chronos/domain/model/Salary.java
+++ core/src/main/java/org/qi4j/chronos/domain/model/Salary.java
@@ -7,35 +7,8 @@
  */
 package org.qi4j.chronos.domain.model;
 
-import org.qi4j.api.mixin.Mixins;
-import org.qi4j.api.injection.scope.PropertyField;
-import org.qi4j.api.injection.scope.This;
-import org.qi4j.library.general.model.Amount;
-import org.qi4j.library.general.model.Currency;
-import org.qi4j.library.general.model.Descriptor;
-import org.qi4j.api.property.ComputedPropertyInstance;
-import org.qi4j.api.property.Property;
+import org.qi4j.library.general.model.HasMoney;
 
-@Mixins( Salary.DescriptorMixin.class )
-public interface Salary extends Amount<Long>, Currency, Descriptor
+public interface Salary extends HasMoney
 {
-    static final class DescriptorMixin implements Descriptor
-    {
-        private @This Salary salary;
-
-        private @PropertyField Property<String> displayValue;
-
-        public Property<String> displayValue()
-        {
-            return new ComputedPropertyInstance<String>( displayValue )
-            {
-                private String m_displayValue = salary.currency().get().getSymbol() + salary.amount().get().toString();
-
-                public String get()
-                {
-                    return m_displayValue;
-                }
-            };
-        }
-    }
 }
